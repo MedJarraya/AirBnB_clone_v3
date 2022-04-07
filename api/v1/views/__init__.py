00a6f5d1b98a878f8b762f09a7d7d4016a68d1f5
Mohamed Jarraya <mohamedjarraya@Mohameds-MacBook-Air.local>
2022-04-07 15:25:12 +0100

    fr

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-"""
-Config all for blueprint appviews
-"""
+""" init for views in v1"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1/')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,25 +1,34 @@
 #!/usr/bin/python3
-""" index file foe view"""
-from flask import jsonify
+"""is the index for app api"""
 from api.v1.views import app_views
+from flask import jsonify
 from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
-@app_views.route('/status')
+@app_views.route('/status', strict_slashes=False, methods=['GET'])
 def status():
-    """ Returns the status of the request """
-    return jsonify({"status": "OK"})
+    """ returns dictionary containing status """
+    return jsonify({'status': "OK"})
 
 
-@app_views.route('/stats')
+@app_views.route('/stats', strict_slashes=False, methods=['GET'])
 def stats():
-    """
-    endpoint that retrieves the number of each objects by type
-    """
-    objects = {"amenities": storage.count("Amenity"),
-               "cities": storage.count("City"),
-               "places": storage.count("Place"),
-               "reviews": storage.count("Review"),
-               "states": storage.count("State"),
-               "users": storage.count("User")}
-    return jsonify(objects)
+    """ returns dictionary with classes and their count"""
+    classes = {
+        "amenities": Amenity,
+        "cities": City,
+        "places": Place,
+        "reviews": Review,
+        "states": State,
+        "users": User
+    }
+    countDict = {}
+    for objType, cls in classes.items():
+        countDict.update({objType: storage.count(cls)})
+    return jsonify(countDict)
