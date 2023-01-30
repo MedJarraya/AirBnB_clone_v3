e81fa6d7bb04666af85babe32f139360620a4f3d
Mohamed Jarraya <mohamedjarraya@Mohameds-MacBook-Air.local>
2023-01-30 11:53:37 +0100

    new

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""sets up our api for state objs"""
+"""sets up our api for state objs @author: @medjarraya"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
